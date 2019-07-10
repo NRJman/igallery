4f96314decee05500672bc9b35055d2a85bd352c
NRJman <vgrynishyn@gmail.com>
2019-07-10 12:17:48 +0300

    add subscriptions controller

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import * as fromApp from './../store/app.reducers';
 import * as fromAuth from './../auth/store/auth.reducer';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Location } from '@angular/common';
 import { BASE_URL_TOKEN } from '../app.config';
+import { Unsubscriber } from '../shared/unsubscriber';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private authState: Observable<fromAuth.State>;
+export class HomeComponent extends Unsubscriber implements OnInit, OnDestroy {
+  private authState$: Observable<fromAuth.State>;
   private clientId: string;
   private authStateSubscription: Subscription;
 
@@ -21,21 +22,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     private location: Location,
     private store: Store<fromApp.State>,
     @Inject(BASE_URL_TOKEN) private baseUrl: string
-  ) { }
+  ) {
+    super();
+  }
 
   signIn(): void {
     window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${this.clientId}&redirect_uri=${this.baseUrl}${this.location.path()}&response_type=token`;
   }
 
   ngOnInit(): void {
-    this.authState = this.store.select('auth');
-    this.authStateSubscription = this.authState.subscribe((authState: fromAuth.State) => {
+    this.authState$ = this.store.select('auth');
+    this.authStateSubscription = this.authState$
+    .pipe(
+      takeUntil(this.subscriptionController$$)
+    )
+    .subscribe((authState: fromAuth.State) => {
       this.clientId = authState.clientId;
     });
   }
 
   ngOnDestroy(): void {
-    this.authStateSubscription.unsubscribe();
+    super.ngOnDestroy();
   }
 
 }
